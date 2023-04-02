19100b669e95e2764befc90fbc9af7d13d2767b9
Usman Gujjar <97385283+usmanfaryadgujjar@users.noreply.github.com>
2023-04-02 16:32:48 +0500

    Update header.php
    vender add to admin -> ./admin

--- login/Seller/header.php
+++ login/Seller/header.php
@@ -5,10 +5,10 @@
     <title>Car Spare Parts</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon.png">
-    <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.theme.default.min.css">
-    <link href="./vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
-    <link href="./css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../admin/vendor/owl-carousel/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../admin/vendor/owl-carousel/css/owl.theme.default.min.css">
+    <link href="../admin/vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
+    <link href="../admin/css/style.css" rel="stylesheet">
 
 
 
